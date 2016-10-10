441a7b2da2b5967331278f859dbf23b9f51c7ecd
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-09 22:44:08 -0400

    Adds field head, no arg constructor, and a method that adds a node to the head

--- List.java
+++ List.java
@@ -1,2 +1,15 @@
 public class List {
+  //fields
+  Node head;
+  
+  //constructors
+  public List() {
+    this.head = null;
+  }
+  
+  //methods
+  //adds a node to the head of the list and passes a String to it
+  public void addNode(String dataInput){
+    this.head = new Node(dataInput);
+  }
 }
