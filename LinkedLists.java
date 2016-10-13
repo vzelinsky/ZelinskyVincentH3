799eb94454cf1789cc4772a684538782f65fbf10
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-13 01:29:11 -0400

    Adds print method and tests outputs
    FIX addNode method

--- LinkedLists.java
+++ LinkedLists.java
@@ -7,5 +7,13 @@ public class LinkedLists
 {
   public static void main (String[] args)
   {
+    List myList = new List();
+    myList.addNode("Anne");
+    myList.addNode("Charlie");
+    myList.printList();
+    
+    Node n1 = new Node("Anne");
+    Node n2 = new Node("Charlie");
+    System.out.println(n1.data+"\n"+n2.data);
   }
 }
--- List.java
+++ List.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
 public class List {
   //fields
   Node head;
-  Scanner scanner = new Scanner(System.in);
   
   //constructors
   public List() {
@@ -11,10 +9,17 @@ public class List {
   
   //methods
   //adds a node to the head of the list and passes a String to it
-  public String addNode() {
-    return scanner.nextLine();
-  }
   public void addNode(String dataInput) {
     this.head = new Node(dataInput);
+    //FIX: just adds node to head and overwrites the previous head
+  }
+  
+  //traverses list and prints
+  public void printList() {
+    Node ref = head;
+    while (ref != null) {
+      System.out.println(ref.data);
+      ref = ref.next;
+    }
   }
 }
