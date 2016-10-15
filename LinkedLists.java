12f417cda1aeec82079cdce192e7af3690001c80
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-15 19:30:06 -0400

    Adds a third node and tests

--- LinkedLists.java
+++ LinkedLists.java
@@ -10,10 +10,12 @@ public class LinkedLists
     List myList = new List();
     myList.addNode("Anne");
     myList.addNode("Charlie");
+    myList.addNode("Earl");
     myList.printList();
     
     Node n1 = new Node("Anne");
     Node n2 = new Node("Charlie");
-    System.out.println(n1.data+"\n"+n2.data);
+    Node n3 = new Node("Earl");
+    System.out.println(n1.data+"\n"+n2.data+"\n"+n3.data);
   }
 }
