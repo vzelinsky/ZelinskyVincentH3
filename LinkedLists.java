c38817cb26666c4e9c57eb3d2e67e86b2907941d
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-15 20:54:29 -0400

    Fixes testing

--- LinkedLists.java
+++ LinkedLists.java
@@ -10,12 +10,8 @@ public class LinkedLists
     List myList = new List();
     myList.addNode("Anne");
     myList.addNode("Charlie");
+    myList.addNode("Barb");
     myList.addNode("Earl");
     myList.printList();
-    
-    Node n1 = new Node("Anne");
-    Node n2 = new Node("Charlie");
-    Node n3 = new Node("Earl");
-    System.out.println(n1.data+"\n"+n2.data+"\n"+n3.data);
   }
 }
