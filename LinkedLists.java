53296015d762ebaec02e2591a62b49ef3045c8f2
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-19 15:01:27 -0400

    Re-adds Node inputs for testing

--- LinkedLists.java
+++ LinkedLists.java
@@ -8,14 +8,16 @@ public class LinkedLists
   public static void main (String[] args)
   {
     List myList = new List();
-    //myList.addNode("Joe");
+    myList.addNode("Joe");
     myList.addNode("Anne");
     myList.addNode("Charlie");
     myList.addNode("Bart");
-    //myList.addNode("Earl");
-    //myList.addNode("Abel");
-    //myList.addNode("Delta");
-    //myList.addNode("Zack");
+    myList.addNode("Earl");
+    myList.addNode("Abel");
+    myList.addNode("Delta");
+    myList.addNode("Zack");
+    myList.printList();
+    System.out.println("---------------------------------");
     myList.printListb();
   }
 }
