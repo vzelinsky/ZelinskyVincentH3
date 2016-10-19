7ad9f01daf3e242e534ec2ddb3e0641ed4062877
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-19 02:06:06 -0400

    Adds prev field and adds to constructors in order to make list doubly linked

--- Node.java
+++ Node.java
@@ -2,16 +2,19 @@ public class Node {
   //fields
   String data;
   Node next;
+  Node prev;
   
   //constructors
   public Node(String dataInput) {
     this.data = dataInput;
     this.next = null;
+    this.prev = null;
   }
   
-  public Node(String dataInput, Node n) {
+  public Node(String dataInput, Node n, Node p) {
     this.data = dataInput;
     this.next = n;
+    this.prev = p;
   }
   
   //methods
