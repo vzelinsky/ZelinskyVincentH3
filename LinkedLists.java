1daec884c84a84ba1c6c0d878d94b19e51159597
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-19 02:05:32 -0400

    Adds more tests for every case of sorting

--- LinkedLists.java
+++ LinkedLists.java
@@ -8,11 +8,14 @@ public class LinkedLists
   public static void main (String[] args)
   {
     List myList = new List();
+    myList.addNode("Joe");
     myList.addNode("Anne");
     myList.addNode("Charlie");
-    myList.addNode("Barb");
+    myList.addNode("Delta");
     myList.addNode("Earl");
     myList.addNode("Abel");
+    myList.addNode("Delta");
+    myList.addNode("Zack");
     myList.printList();
   }
 }
