8be5c85062a5bb1fc819ed915a35abe43f7bb851
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-23 22:15:56 -0400

    All tests are clearly spelled out now

--- LinkedLists.java
+++ LinkedLists.java
@@ -8,23 +8,43 @@ public class LinkedLists
   public static void main (String[] args)
   {
     List myList = new List();
-    myList.addNode("Joe");
-    myList.addNode("Anne");
+    
+    System.out.println("Prints forwards");
+    myList.addNode("Alpha");
+    myList.addNode("Bravo");
     myList.addNode("Charlie");
-    myList.addNode("Bart");
-    myList.addNode("Earl");
-    myList.addNode("Abel");
-    myList.addNode("Delta");
-    myList.addNode("Zack");
-    myList.printListForward();
+    System.out.println("The List forwards: ");
+    myList.printListForward(); //Demonstrates traversing forward and printing
     System.out.println("---------------------------------");
-    myList.printListBackward();
-    //System.out.println(myList.getHead().getData());
-    System.out.println(myList.findNode("Joe"));
-    myList.deleteNode("Zack");
-    System.out.println(myList.findNode("Joe"));
-    myList.printListForward();
+    
+    System.out.println("Add Nodes alphabetically");
+    myList.addNode("Aa");
+    myList.addNode("Foxtrot");
+    myList.addNode("Echo");
+    myList.addNode("Delta");
+    myList.addNode("Zulu");
+    System.out.println("The List: ");
+    myList.printListForward(); //Demonstrates that Nodes add in alphabetical order
+    System.out.println("---------------------------------");
+    
+    System.out.println("Prints backwards");
+    System.out.println("The List backwards: ");
+    myList.printListBackward(); //Demonstrates traversing backwards and printing
+    System.out.println("---------------------------------");
+    
+    System.out.println("Finds Nodes matching a String");
+    System.out.println(myList.findNode("Echo").getData()); //Demonstrates that it can find a Node that matches a String
+    System.out.println("---------------------------------");
+    
+    System.out.println("Deletes Node");
+    myList.deleteNode("Echo");
+    System.out.println("The List: ");
+    myList.printListForward(); //Demonstrates that a Node can be specified and deleted
+    System.out.println("---------------------------------");
+
+    System.out.println("Deletes List");
     myList.deleteList();
-    System.out.println(myList.getHead());
+    System.out.println("Head is "+myList.getHead());
+    System.out.println("Tail is "+myList.getTail()); //Demonstrates that the List can be deleted
   }
 }
